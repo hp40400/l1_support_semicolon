041cf144f14c59efdf062d41355037b3468ab25c
Raheem Mohamed <raheem_mohamed@persistent.com>
2023-02-25 20:29:45 +0530

    Added new switch for select openAI model finetune, embeddings

--- front-end/src/app/layouts/admin-layout/admin-layout.component.ts
+++ front-end/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,35 +1,40 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: "app-admin-layout",
-  templateUrl: "./admin-layout.component.html",
-  styleUrls: ["./admin-layout.component.scss"]
+  selector: 'app-admin-layout',
+  templateUrl: './admin-layout.component.html',
+  styleUrls: ['./admin-layout.component.scss'],
 })
 export class AdminLayoutComponent implements OnInit {
-  public sidebarColor: string = "green";
+  public sidebarColor: string = 'green'
+  choosedAIModelType = 'finetune'
 
   constructor() {}
-  changeSidebarColor(color){
-    var sidebar = document.getElementsByClassName('sidebar')[0];
-    var mainPanel = document.getElementsByClassName('main-panel')[0];
+  changeSidebarColor(color) {
+    var sidebar = document.getElementsByClassName('sidebar')[0]
+    var mainPanel = document.getElementsByClassName('main-panel')[0]
 
-    this.sidebarColor = color;
+    this.sidebarColor = color
 
-    if(sidebar != undefined){
-        sidebar.setAttribute('data',color);
+    if (sidebar != undefined) {
+      sidebar.setAttribute('data', color)
     }
-    if(mainPanel != undefined){
-        mainPanel.setAttribute('data',color);
+    if (mainPanel != undefined) {
+      mainPanel.setAttribute('data', color)
     }
   }
-  changeDashboardColor(color){
-    var body = document.getElementsByTagName('body')[0];
+  changeDashboardColor(color) {
+    var body = document.getElementsByTagName('body')[0]
     if (body && color === 'white-content') {
-        body.classList.add(color);
-    }
-    else if(body.classList.contains('white-content')) {
-      body.classList.remove('white-content');
+      body.classList.add(color)
+    } else if (body.classList.contains('white-content')) {
+      body.classList.remove('white-content')
     }
   }
   ngOnInit() {}
+
+  switchOpenAIModel(modelType) {
+    this.choosedAIModelType = modelType
+    console.log('Model Type', modelType)
+  }
 }
